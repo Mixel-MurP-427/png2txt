606b7a4d88edccf07d8e36ea70472449402831ff
Mixel-MurP-427 <connor.mullarky@outlook.com>
2025-10-27 22:52:25 -0700

    created and completed analyze_png.py got some nice 1x1 and 8x8 PNGs

---
+++ analyze_png.py
@@ -0,0 +1,65 @@
+#use `hex()` to convert to hexadecimal and `chr()` to convert to ascii
+#for the markdown, prefix the code sections with 'd', 'b', 'h', or 'a'
+#TODO add image at bottom of md file
+
+def hex_print(dec_bytes):
+    return ' '.join(hex(byte)[2:] for byte in dec_bytes)
+
+def write_analysis(filepath):
+
+    with open(filepath, 'rb') as myFile:
+        file_bytes = myFile.read()
+
+    int_bytes = tuple(byte for byte in file_bytes) #list of decimal integer values for each byte in the file
+    str_bytes = tuple(str(byte) for byte in int_bytes)
+    byte_index = 0
+    chunk_index = 0
+
+    #begin writing to the markdown file
+    with open('png_bytes_analysis.md', 'w') as md_output:
+
+        #PNG file header
+        md_output.write(f'### PNG file signature\n- Decimal: `{" ".join(str_bytes[:8])}`\n')
+        string_soon = []
+        for byte in int_bytes[:8]:
+            if byte in range(32, 127):
+                string_soon.append(chr(byte))
+            else:
+                string_soon.append(hex(byte))
+        md_output.write(f'- Ascii/hex: `{" ".join(string_soon)}`\n')
+        byte_index += 8
+
+        #go through all chunks
+        while byte_index < len(int_bytes):
+            chunk_index += 1
+            md_output.write(f'### Chunk {chunk_index}\n')
+
+            #chunk length
+            hex_len = hex_print(int_bytes[byte_index:byte_index+4])
+            dec_len = int(''.join(str_bytes[byte_index:byte_index+4]))
+            byte_index += 4
+            md_output.write(f'- **Chunk length:** *h* `{hex_len}`, or {dec_len} bytes.\n')
+
+            #chunk type
+            try: #TODO remove this try thing
+                name = ''.join(chr(byte) for byte in int_bytes[byte_index:byte_index+4])
+                byte_index += 4
+                md_output.write(f'- **Chunk type:** *a* `{name}`\n')
+            except UnicodeEncodeError:
+                print('this ain\'t a chunk title!')
+
+            #chunk data
+            data = hex_print(int_bytes[byte_index:byte_index + dec_len])
+            byte_index += dec_len
+            md_output.write(f'- **Chunk data:** *h* `{data}`\n')
+
+            #chunk CRC checksum
+            crc = hex_print(int_bytes[byte_index:byte_index + 4])
+            byte_index += 4
+            md_output.write(f'- **Chunk CRC:** *h* `{crc}`\n')
+
+        #attach image at bottom
+        md_output.write(f'## The PNG image:\n![the png image]({filepath})')
+
+
+write_analysis('8x8.png')
---
+++ png_bytes_analysis.md
@@ -0,0 +1,25 @@
+### PNG file signature
+- Decimal: `137 80 78 71 13 10 26 10`
+- Ascii/hex: `0x89 P N G 0xd 0xa 0x1a 0xa`
+### Chunk 1
+- **Chunk length:** *h* `0 0 0 d`, or 13 bytes.
+- **Chunk type:** *a* `IHDR`
+- **Chunk data:** *h* `0 0 0 8 0 0 0 8 8 6 0 0 0`
+- **Chunk CRC:** *h* `c4 f be 8b`
+### Chunk 2
+- **Chunk length:** *h* `0 0 0 51`, or 81 bytes.
+- **Chunk type:** *a* `IDAT`
+- **Chunk data:** *h* `78 1 7c 8f 51 e c0 20 8 43 1f 5c dc dd 9c 95 2e f3 c3 a8 84 92 17 b a8 9 54 ab 66 20 92 94 25 75 83 fc 73 a6 c7 db 8f 0 b 10 7e 25 f0 86 50 57 f8 26 58 39 db a8 9e 50 d9 71 72 30 fe f3 ac 47 df 18 5a 31 60 c7 7e 3 97 78 1 0 0 ff ff`
+- **Chunk CRC:** *h* `89 c5 35 51`
+### Chunk 3
+- **Chunk length:** *h* `0 0 0 6`, or 6 bytes.
+- **Chunk type:** *a* `IDAT`
+- **Chunk data:** *h* `3 0 ab ab 36 f9`
+- **Chunk CRC:** *h* `b4 58 9c 26`
+### Chunk 4
+- **Chunk length:** *h* `0 0 0 0`, or 0 bytes.
+- **Chunk type:** *a* `IEND`
+- **Chunk data:** *h* ``
+- **Chunk CRC:** *h* `ae 42 60 82`
+## The PNG image:
+![the png image](8x8.png)
